f53a7058e060db84ec893e81cc1e12187c9d3589
Bohdan Skochii <b.skochii@gmail.com>
2022-07-06 03:20:58 +0300

    Updates

---
+++ precache-manifest.f5faaa407832147799832961c5844424.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0187a2167e04e73f20dd18243a8cddcf",
+    "url": "/01-birthday-reminder-class-components/index.html"
+  },
+  {
+    "revision": "a056a08c4676351e1250",
+    "url": "/01-birthday-reminder-class-components/static/css/main.456f80da.chunk.css"
+  },
+  {
+    "revision": "4df8a0740a3ae6c784e2",
+    "url": "/01-birthday-reminder-class-components/static/js/2.b61ced16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/01-birthday-reminder-class-components/static/js/2.b61ced16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a056a08c4676351e1250",
+    "url": "/01-birthday-reminder-class-components/static/js/main.94faaf18.chunk.js"
+  },
+  {
+    "revision": "83fe886ae5cf379c49b5",
+    "url": "/01-birthday-reminder-class-components/static/js/runtime-main.2a05e0fe.js"
+  }
+]);
